b5b4edba4d8e18cddf0af2fbad97eb3c0c9113d6
Fabius Gasber <fabiusgasber@gmail.com>
2025-05-07 16:34:29 +0200

    feat(Home): add home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom"
+import styles from "./Home.module.css"
+
 function Home() {
 
   return (
-    <>
-    </>
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Ducimus tempora expedita consectetur tenetur veritatis eligendi. 
+          Veritatis laboriosam quos cum optio?
+        </p>
+        <Link className="primaryBtn" to="/shop">Shop now</Link>
+      </section>
+      <img src="/flagship.jpg" alt=""/>
+    </main>
   )
 }
 
