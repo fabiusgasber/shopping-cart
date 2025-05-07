c83cd213f608f8770c42d45249a06f3781ea6ba3
Fabius Gasber <fabiusgasber@gmail.com>
2025-05-07 15:50:43 +0200

    feat(Footer): add footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
+import styles from "./Footer.module.css"
+
 const Footer = () => {
 
     return (
-        <>
-        </>
+        <footer className={styles.footer}>
+            <p>Created by Fabius Gasber</p>
+            <a href="https://github.com/fabiusgasber" target="_blank"><img className={styles.img} src="/github.png"/></a>
+        </footer>
     );
 }
 
