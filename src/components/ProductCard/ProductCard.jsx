bfa3aff08e5a55bc1aa5a3750ee59c6e51b20512
Fabius Gasber <fabiusgasber@gmail.com>
2025-05-07 16:45:31 +0200

    feat(ProductCard): add productcard component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,35 @@
-const ProductCard = () => {
+import { Link } from "react-router-dom";
+import styles from "./ProductCard.module.css"
+import PropTypes from "prop-types";
+
+const ProductCard = ({ style, url, alt, caption, to, item }) => {
 
     return (
-        <>
-        </>
+        <figure className={style ? style : styles.figure}>
+            {
+                to ?
+                <Link to={to} state={ {item: item} } className={styles.link}>
+                <img className={styles.image} src={url} alt={alt}/>
+            </Link> 
+            :
+            <img className={styles.image} src={url} alt={alt}/>
+            }
+        { caption && <figcaption className={styles.figcaption}>
+                <p id={styles.title}>{caption?.text}</p>
+                <p className={styles.price}>{caption?.price} $</p>
+            </figcaption>
+        }
+        </figure>
     );
 }
+
+ProductCard.propTypes = {
+    url: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    caption: PropTypes.shape({
+        text: PropTypes.string,
+        price: PropTypes.string,
+    })
+};
 
 export default ProductCard;
