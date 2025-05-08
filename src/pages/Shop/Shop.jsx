9d0f1bee11cc044a6142eab57926304dfa70791c
Fabius Gasber <fabiusgasber@gmail.com>
2025-05-08 13:37:33 +0200

    feat(Shop): add shop component
    feat(Shop): use custom data hook for shop data
    
    style(Shop): add shop styling
    
    feat(Shop): use error page component  when shop does not load
    
    style(Shop): add shop styling

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,10 +1,20 @@
+import ProductCard from "../../components/ProductCard/ProductCard";
+import styles from "./Shop.module.css"
+import useData from "../../customHooks/useData";
+import ErrorPage from "../ErrorPage/ErrorPage"
+
+
 const Shop = () => {
 
+    const { data, error, loading } = useData("https://fakestoreapi.com/products?limit=12");
+
+    if(loading) return <h1>...Loading items</h1>
+    if(error) return <ErrorPage />
     return (
         <main className={styles.main}>
                 <h1>Our collection</h1>
                 <section className={styles.productSection}>
-                { items.map((item) => (
+                { data.map((item) => (
                     <ProductCard item={item} to={`/product/${item.id}`} key={item.id} url={item.image} alt={item.title} caption={{ text: item.title, price: item.price }} />
                 )) }
                 </section>
