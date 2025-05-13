a8aac620639a5231d464e02a5d09833350125338
Fabius Gasber <fabiusgasber@gmail.com>
2025-05-13 13:45:04 +0200

    feat(useCartItems): create custom hook to handle cart items

---
+++ src/useCartItems.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const useCartItems = () => {
+        const [cartItems, setCartItems] = useState([]);
+            
+        const addToBag = (e, item, quantity) => {
+            e.preventDefault();
+            const duplicate = cartItems.filter((elem) => (elem.id === item.id));
+            duplicate.length ?
+            setCartItems(cartItems.map((elem) => elem.id === item.id ? { ...item, quantity: quantity } : elem)) 
+            : 
+            setCartItems([...cartItems, { ...item, quantity: quantity }]);
+        }
+
+        const deleteItem = (item) => {
+            setCartItems(cartItems.filter(elem => elem.id !== item.id));
+        }
+    
+    return { cartItems, addToBag, deleteItem }
+    
+}
+
+export default useCartItems;
