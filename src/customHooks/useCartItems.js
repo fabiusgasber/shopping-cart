caffb5f72819720186059851d7183752c34c2550
Fabius Gasber <fabiusgasber@gmail.com>
2025-05-16 10:51:44 +0200

    feat(useCartItems): add tests for custom hook useCartItems

--- src/customHooks/useCartItems.js
+++ src/customHooks/useCartItems.js
@@ -4,7 +4,7 @@ const useCartItems = () => {
         const [cartItems, setCartItems] = useState([]);
             
         const addToBag = (e, item, quantity) => {
-            e.preventDefault();
+            if(e instanceof Event) e.preventDefault();
             const duplicate = cartItems.filter((elem) => (elem.id === item.id));
             duplicate.length ?
             setCartItems(cartItems.map((elem) => elem.id === item.id ? { ...item, quantity: quantity } : elem)) 
